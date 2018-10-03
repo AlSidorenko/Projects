2398dc9250a686c140172033b0378d8f3290c07b
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-10-03 17:30:00 +0300

    Edit code. Отсортировать департаменты [#34352]

--- Project/src/main/java/ru/classes/sortdepartments/Departments.java
+++ Project/src/main/java/ru/classes/sortdepartments/Departments.java
@@ -2,6 +2,7 @@ package ru.classes.sortdepartments;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.TreeSet;
 
 /**
  * Created on 27.09.2018.
@@ -14,10 +15,11 @@ public class Departments {
 
     /**
      * Sort the list in ascending order.
+     *
      * @param list - input list.
      * @return - sort list.
      */
-    ArrayList<String> sortNatural(ArrayList<String> list) {
+    public ArrayList<String> sortNatural(ArrayList<String> list) {
         checker(list);
         list.sort(
                 new Comparator<String>() {
@@ -32,29 +34,26 @@ public class Departments {
 
     /**
      * Sort the list in descending order.
+     *
      * @param list - input list.
      * @return - sort list.
      */
-    ArrayList<String> sortReverse(ArrayList<String> list) {
+    public ArrayList<String> sortReverse(ArrayList<String> list) {
         checker(list);
         list.sort(
                 new Comparator<String>() {
                     @Override
                     public int compare(String o1, String o2) {
                         int result;
-                        if (o1.length() == o2.length()) {
-                            result = o2.compareTo(o1);
+                        int size = Math.min(o1.length(), o2.length());
+                        String first = o1.substring(0, size);
+                        String second = o2.substring(0, size);
+                        if (first.compareTo(second) == 0 && o1.length() > o2.length()) {
+                            result = 1;
+                        } else if (first.compareTo(second) == 0 && o1.length() < o2.length()) {
+                            result = -1;
                         } else {
-                            int size = Math.min(o1.length(), o2.length());
-                            String first = o1.substring(0, size);
-                            String second = o2.substring(0, size);
-                            if (first.compareTo(second) == 0 && o1.length() > o2.length()) {
-                                result = 1;
-                            } else if (first.compareTo(second) == 0 && o1.length() < o2.length()) {
-                                result = -1;
-                            } else {
-                                result = o2.compareTo(o1);
-                            }
+                            result = o2.compareTo(o1);
                         }
                         return result;
                     }
@@ -65,11 +64,12 @@ public class Departments {
 
     /**
      * Checking and adding missing departments.
+     *
      * @param list - input list.
      * @return - Full List.
      */
-    ArrayList<String> checker(ArrayList<String> list) {
-        ArrayList<String> res = new ArrayList<>();
+    public TreeSet<String> checker(ArrayList<String> list) {
+        TreeSet<String> res = new TreeSet<>();
         for (int i = 0; i < list.size(); i++) {
             String[] separated = list.get(i).split("\\\\");
             if (separated.length > 1) {
